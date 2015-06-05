6e1169e63635c77d8bc3af33727f4e1efa108419
George Tsaousidis <georgetsd@MacBook-Pro-toutes-George.local>
2015-06-05 16:59:15 +0300

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by George Tsaousidis on 5/6/15.
+//  Copyright (c) 2015 DFG-Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Ribbit
+//
+//  Created by George Tsaousidis on 5/6/15.
+//  Copyright (c) 2015 DFG-Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
